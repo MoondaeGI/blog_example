0fb689e574c4a08fa68b8be4eb73697703ec465f
MoondaeGI <ansgur119977@gmail.com>
2023-06-29 01:34:57 +0900

    WebSecurityConfig의 이름 변경, annotation 재설정

---
+++ src/main/java/com/example/blog_example/util/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.blog_example.util.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
